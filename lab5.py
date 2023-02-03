b50a9688c69edf98fe82cee5be359032dfbd0396
Harsh Baraiya <baraiyaharsh319@gmail.com>
2023-02-03 11:19:57 +0530

    lab5 add

---
+++ lab5.py
@@ -0,0 +1,22 @@
+a =[1,2,3,4,5,6,7,8,9,10]
+for i in a:
+    if i<5 :
+        print(i)
+
+
+a = [1, 1, 2, 3, 5, 5, 5, 6, 7, 8, 9]
+print( [i for i in a if i < 10])
+
+
+
+
+a = int(input("enter first number:"))
+
+
+if a%4==0:
+    print("even number:")
+if a%2==0:
+    print("print even:")
+else:
+    print("odd number:")
+
