4dee3dd0fea819b404258a02884648c506d6b9d6
Harsh Baraiya <baraiyaharsh319@gmail.com>
2023-02-03 11:22:25 +0530

    updated lab5

--- lab5.py
+++ lab5.py
@@ -1,14 +1,16 @@
+'''list'''
+
 a =[1,2,3,4,5,6,7,8,9,10]
 for i in a:
     if i<5 :
         print(i)
 
-
+'''list'''
 a = [1, 1, 2, 3, 5, 5, 5, 6, 7, 8, 9]
 print( [i for i in a if i < 10])
 
 
-
+'''even and odd'''
 
 a = int(input("enter first number:"))
 
